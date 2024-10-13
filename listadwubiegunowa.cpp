90d6411716d566eb560c7f73cbbbab7d13842aac
ShiroKitsu <93395807+Bercik1287@users.noreply.github.com>
2024-10-13 19:27:37 +0200

    stworzenie listy dwukierunkowej

--- listadwubiegunowa.cpp
+++ listadwubiegunowa.cpp
@@ -2,6 +2,24 @@
 
 using namespace std;
 
+struct Wezel {
+    int dane;
+    Wezel* poprzedni;
+    Wezel* nastepny;
+    
+    Wezel(int wartosc) : dane(wartosc), poprzedni(nullptr), nastepny(nullptr) {}
+};
+
+class ListaDwukierunkowa {
+private:
+    Wezel* przod;
+    Wezel* tyl;
+    
+public:
+    ListaDwukierunkowa() : przod(nullptr), tyl(nullptr) {}
+    
+}
+
 int main() {
     
     return 0;
