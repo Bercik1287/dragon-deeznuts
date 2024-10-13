f949feea7920dc94b05c446b69bef58c1d53f50d
ShiroKitsu <93395807+Bercik1287@users.noreply.github.com>
2024-10-13 19:30:01 +0200

    dodawanie do listy
    dodanie funkcji dodających dane do listy

--- listadwubiegunowa.cpp
+++ listadwubiegunowa.cpp
@@ -18,6 +18,51 @@ private:
 public:
     ListaDwukierunkowa() : przod(nullptr), tyl(nullptr) {}
     
+    void dodajZPrzodu(int wartosc) {
+        Wezel* Wezel2 = new Wezel(wartosc);
+        if (!przod) {
+            przod = tyl = Wezel2;
+        } else {
+            Wezel2->nastepny = przod;
+            przod->poprzedni = Wezel2;
+            przod = Wezel2;
+        }
+    }
+
+void dodajZTylu(int wartosc) {
+        Wezel* Wezel2 = new Wezel(wartosc);
+        if (!tyl) {
+            przod = tyl = Wezel2;
+        } else {
+            Wezel2->poprzedni = tyl;
+            tyl->nastepny = Wezel2;
+            tyl = Wezel2;
+        }
+    }
+
+void dodajDoIndeksu(int indeks, int wartosc) {
+        if (indeks == 0) {
+            dodajZPrzodu(wartosc);
+            return;
+        }
+        Wezel* obecny = przod;
+        int obecnyIndeks = 0;
+        while (obecny && obecnyIndeks < indeks) {
+            obecny = obecny->nastepny;
+            obecnyIndeks++;
+        }
+        if (!obecny) {
+            dodajZTylu(wartosc);
+        } else {
+            Wezel* Wezel2 = new Wezel(wartosc);
+            Wezel2->poprzedni = obecny->poprzedni;
+            Wezel2->nastepny = obecny;
+            if (obecny->poprzedni) {
+                obecny->poprzedni->nastepny = Wezel2;
+            }
+            obecny->poprzedni = Wezel2;
+        }
+    }
 }
 
 int main() {
